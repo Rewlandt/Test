ae4148c6a5587ddcb97cefc25f1436324667ed08
Rewlandt <101276693+Rewlandt@users.noreply.github.com>
2023-11-20 13:26:58 +0300

    Delete task1/php_interface directory

--- task1/php_interface/include/lang/ru/events.php
+++
@@ -1,4 +0,0 @@
-<?
-$MESS["TASK2_AUTH_USER"] = "Пользователь авторизован: #ID# (#LOGIN#) #NAME#, данные из формы: #NAME_FORM#";
-$MESS["TASK2_NO_AUTH_USER"] = "Пользователь не авторизован, данные из формы: #NAME_FORM#";
-$MESS["TASK2_REPLACEMENT"] = "Замена данных в отсылаемом письме";
--- task1/php_interface/init.php
+++
@@ -1,3 +0,0 @@
-<?
-require_once $_SERVER["DOCUMENT_ROOT"] . '/bitrix/php_interface/include/const.php';
-require_once $_SERVER["DOCUMENT_ROOT"] . '/bitrix/php_interface/include/events.php';
