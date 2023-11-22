4f2a6a020bdf134498834ee1cc2e0d419d16c563
Rewlandt <101276693+Rewlandt@users.noreply.github.com>
2023-11-22 10:49:38 +0300

    Update index.php

--- task2/index.php
+++ task2/index.php
@@ -4,5 +4,11 @@ $APPLICATION->SetTitle("Задание2");
 ?><?$APPLICATION->IncludeComponent(
 	"simplecomp.catalog",
 	"",
-Array()
+	Array(
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"NEWS_IBLOCK_ID" => "1",
+		"PRODUCTS_IBLOCK_ID" => "2",
+		"PRODUCTS_IBLOCK_ID_PROPERTY" => "UF_NEWS_LINK"
+	)
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
